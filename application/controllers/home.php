c49468f50b0f257e15f7f5c4b0b3e150b09aba34
Hurin <hurin@live.ca>
2014-05-18 00:55:21 +0800

    add front-end

---
+++ application/controllers/home.php
@@ -0,0 +1,52 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('home');
+	}
+	
+	public function about()
+	{
+		$this->load->view('index-1');
+	}
+	
+	public function services()
+	{
+		$this->load->view('index-2');
+	}
+	
+	public function projects()
+	{
+		$this->load->view('index-3');
+	}
+	
+	public function blog()
+	{
+		$this->load->view('index-4');
+	}
+	
+	public function contacts()
+	{
+		$this->load->view('index-5');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
